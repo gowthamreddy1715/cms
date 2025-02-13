234fa1335df616205862d799428bd875f4353445
gowthamreddy1715 <nallapareddygowtham@gmail.com>
2025-02-13 22:28:49 +0530

    commited

--- Backend/Routes/goUsers.js
+++ Backend/Routes/goUsers.js
@@ -1,13 +1,17 @@
-const express = require('express');
-const { registerUser,loginUser,getUser,editUser } = require('../Controller/UserController');
-const {protect} = require('../Middleware/authMiddleware')
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  getUser,
+  editUser,
+} = require("../Controller/UserController");
+const { protect } = require("../Middleware/authMiddleware");
 
 const router = express.Router();
 
-
-router.post('/',registerUser)
-router.post('/login',loginUser)
-router.get('/me',protect, getUser)
-router.put('/edit',protect,editUser)
+router.post("/", registerUser);
+router.post("/login", loginUser);
+router.get("/get", protect, getUser);
+router.put("/put", protect, editUser);
 
 module.exports = router;
